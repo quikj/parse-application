6f2deca80c787c3f2ed126d00486b4ce0a1d2549
quikj <gyasi.jordan@bison.howard.edu>
2015-10-01 10:05:33 -0400

    Create credentials.js

---
+++ credentials.js
@@ -0,0 +1,7 @@
+/**
+ * Created by quikj on 9/24/15.
+ */
+PARSE__HEADER_CREDENTIALS = {
+    "x-parse-application-id": "ts3se7CyEn0nxuA4D09c1xencH7VLebX82zsnc2i",
+    "x-parse-rest-api-key": "O81z7gDbEhpBK9TkdCA9OssG5yOI8dBwItOGhD9s"
+};
