c1d6fb453fe614e3ec55481f7e9f4fe9c7b874a1
quikj <gyasi.jordan@bison.howard.edu>
2015-10-14 16:20:23 -0400

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -4,104 +4,22 @@
 /**
  * Created by quikj on 9/23/15.
  */
-myApp = angular.module('firstApp',['ionic','App services']);
-    myApp.config(function($stateProvider, $urlRouterProvider) {
+myApp = angular.module('firstApp',['ionic','AppControllers','AppServices']);
 
-        $stateProvider
-            .state('home', {
-                url: "/home",
-                templateUrl: "index.html",
-                controller: "FirstController"
-            })
-            .state('detail', {
-                url: "/detail",
-                templateUrl: "detail.html",
-                controller: "detailCtrl"
-            });
-    })
-    .controller('FirstController', function($scope,ParseHttpService) {
-        $scope.myName = "Gyasi Jordan";
+myApp.config(function($stateProvider, $urlRouterProvider) {
 
-        $scope.itemsList = [];
-        $scope.inputItem = {
-            value: "",
-            name: "",
-            room: ""
-        };
-        /*called when user clicks the ADD button*/
-        $scope.addItem = function addItem() {
-            var data = $scope.inputItem.value.split(",");
-            if (data.length === 2) {
-                $scope.inputItem.name = data[0].trim();
-                $scope.inputItem.room = data[1].trim();
+    $urlRouterProvider.otherwise("/home");
+    $stateProvider
 
-                ParseHttpService.addObject($scope.inputItem)
-                    .then(function itemSaved(_newItem) {
-                        alert("Item Saved", _newItem.objectId);
-                        $scope.inputItem = {}; //clear the inputItem object
-
-                        return populateList(); //update the list
-
-                    }, function errorSaving(_error) {
-                        $scope.inputItem = {};
-                    });
-            }
-        };
-        $scope.editObject = function editObject(_object) {
-            var data = null;
-            var editedObject = {};
-            var objectData = prompt("Please enter the updated information for:", _object.name);
-
-            if (objectData !== null) {
-                data = objectData.split(",");
-            }
-            if (objectData && (data.length === 2)) {
-                editedObject.name = data[0].trim();
-                editedObject.room = data[1].trim();
-                editedObject.objectId = _object.objectId;
-
-                console.log(JSON.stringify(editedObject));
-                ParseHttpService.updateObject(editedObject)
-                    .then(function itemUpdated(_updatedItem) {
-                        alert("Item Updated " + _updatedItem.objectId)
-                        return populateList();
-                    }, function errorSaving(_error) {
-                        alert("Error Editing Object " + _error)
-                    });
-            } else
-                if (objectData !== null) {
-                    alert("Invalid input " + objectData);
-
-                }
-
-            };
-        $scope.deleteObject = function deleteObject(_objectId) {
-            ParseHttpService.deleteObjectById(_objectId)
-                .then(function itemSaved(_deletedObject) {
-                    alert("Item Deleted " + _deletedObject.objectId);
-
-                    return populateList();
-
-                }, function errorDeleting(_error) {
-                    alert("Error Deleting Object " + _objectId)
-                });
-        };
-
-        /*gets a list of items in Parse account*/
-        function populateList() {
-            ParseHttpService.getAllObjects().then(function (_listData) {
-                $scope.itemsList = _listData.results;
-            });
-        }
-        /*calls the login fxn, shows which user logged in and gets the list from Parse*/
-        ParseHttpService.login().then(function (_loggedInUser) {
-            alert("User logged in " + _loggedInUser.username);
-            return populateList();
+        .state('home', {
+            url: "/home",
+            templateUrl: "views/home.html",
+            controller: "homeCtrl"
         })
-    })
-    .controller('detailCtrl', function ($scope, $state) {
-        $scope.stateInfo = $state.current;
-        $scope.params = $state.params;
-    });
-
+        .state('detail', {
+            url: "detail/:objectId",
+            templateUrl: "views/detail.html",
+            controller: "detailCtrl"
+        });
+});
 
