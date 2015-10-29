f55995c545fbf33f0f9a59d82eb26c4268ad7ef0
quikj <gyasi.jordan@bison.howard.edu>
2015-10-29 16:27:03 -0400

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -8,18 +8,47 @@ myApp = angular.module('firstApp',['ionic','AppControllers','AppServices']);
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise("/home");
+    function checkForAuthenticatedUser(ParseService, $state) {
+        return ParseService.getCurrentUser().then(function (_user) {
+            // if resolved successfully return a user object that will set
+            // the variable `resolvedUser`
+            return _user;
+        }, function (_error) {
+            $state.go('login');
+        })
+    }
+
+    $urlRouterProvider.otherwise("app/home");
     $stateProvider
 
-        .state('home', {
+        .state('login', {
+            url: "/login",
+            templateUrl: "views/login.html",
+            controller: 'loginCtrl'
+        })
+        .state('app', {
+            url: "/app",
+            templateUrl: '<ion-nav-view></ion-nav-view>',
+            abstract: true,
+            resolve : {
+                resolvedUser : checkForAuthenticatedUser
+            }
+        })
+        .state('app.home', {
             url: "/home",
             templateUrl: "views/home.html",
-            controller: "homeCtrl"
+            controller: "homeCtrl",
+            resolve: {
+                CurrentUser: function(resolvedUser){
+                    return resolvedUser;
+                }
+            }
         })
-        .state('detail', {
-            url: "detail/:objectId",
+        .state('app.detail', {
+            url: "app/detail/:objectId",
             templateUrl: "views/detail.html",
             controller: "detailCtrl"
         });
 });
+
 
