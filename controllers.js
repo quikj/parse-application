c1fa79fddeea1d09283bff31ebd8b97258662358
quikj <gyasi.jordan@bison.howard.edu>
2015-10-29 16:26:00 -0400

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,5 +1,5 @@
  angular.module('AppControllers', ['AppServices'])
-        .controller('homeCtrl', function ($scope, $state, ParseHttpService) {
+        .controller('homeCtrl', function ($scope, $state, $timeout, ParseHttpService, CurrentUser) {
             $scope.stateInfo = $state.current;
             $scope.myName = "Gyasi Jordan";
 
@@ -89,4 +89,21 @@
                 $scope.item = _data;
                 $scope.name = $scope.item.name;
             })
-        });
+        })
+     .controller('loginCtrl', function ($scope, $state, ParseService, $timeout) {
+         console.log("In Login Controller");
+
+         $scope.credentials = {};
+
+         $scope.doLoginAction = function () {
+             ParseService.login($scope.credentials).then(function (_user) {
+                 $timeout(function () {
+                     $state.go('app.home', {});
+                     console.log("user", _user);
+                 }, 2);
+
+             }, function (_error) {
+                 alert("Login Error " + (_error.message ? _error.message: _error.data.error));
+             });
+         }
+     });
